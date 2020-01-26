dea7e0e63727c85b6a159054ec1960adc5c78c62
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2020-01-26 10:25:11 -0500

    adding testing js file

---
+++ strings_substring.js
@@ -0,0 +1,6 @@
+// Write code that finds the index of the letter “j” in My favorite dessert is jello.
+
+let myFavoriteFood = "My favorite dessert is jello."
+
+let indexOfLetterJ = myFavoriteFood.lastIndexOf("j")
+console.log(`the index of the letter j is ${indexOfLetterJ}`)
